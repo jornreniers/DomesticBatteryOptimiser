e89e7912eb04e51f082b68a200f8d226dbddb0cc
Jorn Reniers <jorn_reniers@hotmail.com>
2026-02-12 18:30:15 +0000

    investigate noise in gp

--- src/regression/gaussian_process_regression.py
+++ src/regression/gaussian_process_regression.py
@@ -49,17 +49,53 @@ def gaussian_process_regression(
         config.df.select(config.get_features()).to_numpy(), return_std=True
     )
 
-    # # The uncertainty the GP returns is for what it thinks is the "true" value
-    # # after accounting for "measurement error or noise".
-    # # What we supplied as "noise" is however not measurement error but
-    # # reflects random variations in the actual value. When forecasting values, we should
-    # # therefore add it to the uncertainty, so we can check whether the
-    # # value (ie what the GP thinks is truth + noise) is within
-    # # the forecasted range.
-    # # Ie (y_pred, y_std) is the distribution of the "true" value
-    # # while we are interested in the "noisy" value
-    # # so we add the "noise" to the standard deviation
-    # y_std = y_std + pow(10, noise)
+    """
+    About standard deviation and noise:
+
+    The gaussian process tries to fit the "true underlying" value of y
+    while we supply a noisy measurement y'. The assumption is that
+    y' ~ N(y, sqrt(alpha)). ie alpha is the variance (not std) on measurements.
+    In the documentation, they say alpha is the "value added to the diagnonal"
+    and if you check the equations, this is std^2.
+
+    What the gp returns with the predict-function, is the distribution of 
+    the true measurement y, ie its mean and std based on how well certain
+    the fit is. Ie it forecasts y ~ N(mu,std), or N(y_pred, y_std)
+
+    Therefore, if we want to compare the predicted distribution with the
+    measurements, we need to add the uncertainties (squared)
+    ince y' ~ N(y,sqrt(alpha)) ~ N(N(y_pred, y_std)), sqrt(alpha))
+    So if we want to get the total standard deviation to y', 
+    std_gp_to = sqrt(std_true_value^2 + std_measurement_noise^2)
+    or with our notation:
+    y_std = sqrt(y_std^2 + alpha)
+
+
+    DETAILS THROUGH CLAUDE:
+    Claude says y_std returns the distribution of the latent (true) values
+    not measured ones. to get the uncertainty on measurements, the equation is
+        measurement_std = sqrt( y_std^2 + alpha)
+    claude calls y_std the epistemic uncertainty, and alpha the aleatoric uncertainty
+    it says they are independent so combine in squared
+    as reference it points to the paper that SKlearn uses
+        https://gaussianprocess.org/gpml/chapters/RW.pdf
+        claude has the wrong numbers for equations but othewise is correct
+        see pages 16-19, eg eqn 2.20 asnd 2.24 where it states
+        that the distribution of noisy measurements has a covariance
+        of K(x,x) + sigma_n^2, where sigma_n == alpha (or ^2)
+    """
+
+    # TODO update code
+    #   the square and sqrt should be element-wise
+    y_std = np.sqrt(y_std**2 + pow(10, noise))
+
+    # TODO WHEN SCORING a hyperparameter fitting, account for the noise
+    #       ie is indeed 68% within +-1 std (y_std+10^alpha)???
+    #       and then minimise alpha while keeping that constraint true?????
+    # ie fit with alpha
+    #   count points with error within +-1std vs those outside
+    #   minimise alpha subject_to err_within_1std >=0.685 * number_of_points
+    #       or also look at 2stds
 
     # Score how well the fit went, plot if desired
     prefix = figname_prefix + f"gaussian_process_optimal_alpha{noise}_"
